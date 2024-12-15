ef5c1211ab5a09698bfbfa4d8d9636f1488f348e
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-12-15 12:57:40 +0530

    update in route

--- index.php
+++ index.php
@@ -3,29 +3,4 @@
 
 require 'config.php';
 require 'function.php';
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php'
-];
-
-function routeToController($uri, $routes)
-{
-    if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
-    } else {
-        abort();
-    }
-}
-
-function abort($code = 404)
-{
-    http_response_code($code);
-    require 'views/404.php';
-    die();
-}
-
-routeToController($uri, $routes);
+require 'routes.php';

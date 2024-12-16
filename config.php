84faa48a4a2da294fda00c85f9ff06ec4a208900
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-12-16 13:34:21 +0530

    connection database

--- config.php
+++ config.php
@@ -1,3 +1,21 @@
 <?php
 
 define('URL_ROOT', '/basicphp/demo');
+
+if ($_SERVER['HTTP_HOST'] === "localhost") {
+    return $config = [
+        'database' => [
+            'host' => 'srv786.hstgr.io',
+            'dbname' => 'u705875743_demo',
+            'charset' => 'utf8mb4'
+        ]
+    ];
+} else {
+    return $config = [
+        'database' => [
+            'host' => 'srv786.hstgr.io',
+            'dbname' => 'u705875743_demo',
+            'charset' => 'utf8mb4'
+        ]
+    ];
+}
--- index.php
+++ index.php
@@ -4,3 +4,16 @@
 require 'config.php';
 require 'function.php';
 require 'routes.php';
+require 'Database.php';
+
+$db = new Database($config['database'], "u705875743_demo", "!>1xnMrV7p6A");
+
+$id = $_GET['id'];
+$query = "SELECT * FROM users where id = :id";
+
+$posts = $db->query($query, ['id' => $id])->fetchAll();
+
+// $query = "SELECT * FROM users";
+// $posts = $db->query($query)->fetchAll();
+
+dd($posts);

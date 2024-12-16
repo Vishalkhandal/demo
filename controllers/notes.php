c1b17a1ede543ce30344c96dd5f8143c50d0b59b
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-12-16 14:11:35 +0530

    connection database

--- controllers/notes.php
+++ controllers/notes.php
@@ -3,7 +3,7 @@
 // $db = new Database($config['database'], "u705875743_demo", "!>1xnMrV7p6A");
 // var_dump($db);
 
-// $heading = "My Notes";
+$heading = "My Notes";
 
 // // $id = $_GET['id'];
 // $id = 3;

16a3ba7b7e54f7fe778d05a055b89a0ef4325419
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-12-16 14:05:24 +0530

    connection database

--- controllers/notes.php
+++ controllers/notes.php
@@ -1,11 +1,22 @@
 <?php
 
+echo "hi";
+
 $db = new Database($config['database'], "u705875743_demo", "!>1xnMrV7p6A");
 
 $heading = "My Notes";
 
-$notes = $db->query("select * from notes where user_id = 1")->fetchAll();
+// $id = $_GET['id'];
+$id = 3;
+$query = "SELECT * FROM notes where user_id = :id";
+
+$notes = $db->query($query, ['id' => $id])->fetchAll();
 
 dd($notes);
 
 require('views/notes.view.php');
+
+// $id = $_GET['id'];
+// $query = "SELECT * FROM users where id = :id";
+
+// $posts = $db->query($query, ['id' => $id])->fetchAll();

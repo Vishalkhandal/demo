78328504fec2d3973ad6059cd80da75af0e2edb3
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-12-27 20:47:29 +0530

    update in routes part

--- controllers/notes.php
+++ controllers/notes.php
@@ -2,16 +2,26 @@
 
 $heading = "My Notes";
 
-$con = [
-    'database' => [
-        'host' => 'srv786.hstgr.io',
-        'dbname' => 'u705875743_demo',
-        'charset' => 'utf8mb4'
-    ]
-];
+if ($_SERVER['HTTP_HOST'] === "localhost") {
+    $con = [
+        'database' => [
+            'host' => 'localhost',
+            'dbname' => 'my_demo',
+            'charset' => 'utf8mb4'
+        ]
+    ];
+} else {
+    $con = [
+        'database' => [
+            'host' => 'srv786.hstgr.io',
+            'dbname' => 'u705875743_demo',
+            'charset' => 'utf8mb4'
+        ]
+    ];
+}
 
 if ($_SERVER['HTTP_HOST'] === "localhost") {
-    $db = new Database($config['database']);
+    $db = new Database($con['database']);
     echo "database connected on local";
 } else {
     $db = new Database($con['database'], "u705875743_demo", "!>1xnMrV7p6A");

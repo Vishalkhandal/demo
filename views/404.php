6524ea3011d0f8e65de21fd56ff7d87cc55f514f
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2025-01-03 10:57:32 +0530

    add registration files

--- views/404.php
+++ views/404.php
@@ -5,9 +5,17 @@ require('partials/nav.php');
 ?>
 
 <main class="container">
-    <div class="bg-body-tertiary p-5 rounded">
-        <h1>Page not found</h1>
-        <a href="/" class="text-primary">back to home</a>
+    <div class="d-flex h-100 text-center justify-content-center">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <main class="px-3">
+                <h1 class="display-3">403</h1>
+                <h2 class="display-6 text-uppercase">ERROR</h2>
+                <p class="lead">Oops! Page not found.</p>
+                <p class="lead">
+                    <a href="/" class="btn btn-lg btn-dark fw-bold">Back to Home</a>
+                </p>
+            </main>
+        </div>
     </div>
 </main>
 
